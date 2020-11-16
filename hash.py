7af9d8248354ece8088842930cf3c46e90bce67d
Biobop <69711600+Biobop@users.noreply.github.com>
2020-11-16 12:17:11 +0800

    Deleted duplicate files

--- hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def encrypt_string(hash_string):
-    sha_signature = \
-        hashlib.sha256(hash_string.encode()).hexdigest()
-    return sha_signature
-
-print(encrypt_string("LIB" + "1"))
